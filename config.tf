975547cc45beeb7a46726c6f73fc336f82d4a259
m.shcherbina <mihail.shcherbina@gmail.com>
2022-09-15 00:12:27 +0300

    2

--- config.tf
+++ config.tf
@@ -16,6 +16,7 @@ resource "aws_instance" "build" {
   ami = "ami-08d4ac5b634553e16"
   instance_type = "t2.micro"
   key_name = "my-key200"
+  security_groups = ["EC2SecurityGroup359"]
 
 
 }
@@ -24,6 +25,7 @@ resource "aws_instance" "stage" {
   ami = "ami-08d4ac5b634553e16"
   instance_type = "t2.micro"
   key_name = "my-key200"
+  security_groups = ["EC2SecurityGroup359"]
 
 }
 
